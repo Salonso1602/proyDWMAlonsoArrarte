b250e27a30f1e837e735aedb23b70212d81b45e0
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2022-10-21 00:30:20 -0300

    Pretty much done with news feed

--- hoteLink/src/app/services/news.service.ts
+++ hoteLink/src/app/services/news.service.ts
@@ -7,12 +7,13 @@ import { catchError, Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class NewsService {
+  private static readonly _apiUrl = 'api/news';
 
   constructor(
     private http: HttpClient
   ) { }
 
   getNews(): Observable<INews[]> {
-    return this.http.get<INews[]>('/api/news');
+    return this.http.get<INews[]>(NewsService._apiUrl);
   }
 }
