a4fe1624d02bd5271ac35666d2927b5141993cd7
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2022-11-12 14:39:31 -0300

    prettier footer, tested cards of more info

--- hoteLink/src/app/Components/moreInfo/more-info-card/more-info-card.component.ts
+++ hoteLink/src/app/Components/moreInfo/more-info-card/more-info-card.component.ts
@@ -13,6 +13,21 @@ export class MoreInfoCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.hotel = {
+      id : '0',
+      name : 'Duncan Hill Hotel',
+      location : {
+        id : '0',
+        name : 'Cochabamba',
+        shortDescription : 'El más hermoso valle de Bolivia',
+        longDescription : "Bla bla bla, info del hotel, bla bla bla, es muy lindo, bla bla bla, fundado en 1475, bla bla bla, premio 6 estrellas Instalaciones: cuenta con psicina, interna y externa, bla bla bla, tambien hay salida a la playa (100% real), bla bla, habitaciones suite con 2 lavabos, bla bla bla, restoran 3 estrellas michelin, buffet y desayuno, bla bla bla Información de contacto Información de contacto Información de contacto para reservas 480912481094 para servicio a habitacion 23141924234 para sugerencias 4309842309"
+      },
+      address : 'Blvar Artigas 2018',
+      attentionHours : '10:00 - 18:00',
+      contactInfo : '+598 99 223 454',
+      shortDescription : 'El hotel cafetero número 1 de la región',
+      longDescription : 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    }
   }
 
 }
