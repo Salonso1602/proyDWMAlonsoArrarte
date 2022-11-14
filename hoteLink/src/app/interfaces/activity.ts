984f914b9b9a2a20771a3a184a044ad7ec74acf7
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2022-11-13 22:19:47 -0300

    Refactor DB activities, global grey colors

--- hoteLink/src/app/interfaces/activity.ts
+++ hoteLink/src/app/interfaces/activity.ts
@@ -2,10 +2,12 @@ import { IBookable } from '@interfaces/bookable';
 import { DaysOfWeek } from '@enums/days-of-week';
 
 export interface IActivity extends IBookable {
-  monthlyPrice: number;
-  timesOfActivity: {
-    day: DaysOfWeek,
-    startTime: string,
-    endTime: string
-  }[];
+  weeklyPrice: number;
+  timesOfActivity: TimeOfActivity[];
+}
+
+export interface TimeOfActivity {
+  day: DaysOfWeek,
+  startTime: Date,
+  endTime: Date
 }
--- hoteLink/src/app/services/in-memory-data-sources/activities.ts
+++ hoteLink/src/app/services/in-memory-data-sources/activities.ts
@@ -1,21 +1,23 @@
 import { DaysOfWeek } from "@enums/days-of-week";
 import { IActivity } from "@interfaces/activity";
 
+import { createTime } from "@utils/date";
+
 export const activities: IActivity[] = [
   {
     id: 1,
     name: 'Clases de equitación: Nivel principiante',
     description: 'Excelente oportunidad para conocer el mundo del caballo',
     place: 'Calle 123',
-    monthlyPrice: 100,
+    weeklyPrice: 100,
     booking: {
       total: 10,
       remaining: 5
     },
     timesOfActivity: [{
       day: DaysOfWeek.Monday,
-      startTime: '10:00',
-      endTime: '11:00'
+      startTime: createTime(10),
+      endTime: createTime(11)
     }]
   },
   {
@@ -23,15 +25,22 @@ export const activities: IActivity[] = [
     name: 'Clases de equitación: Nivel intermedio',
     description: 'Adentrarse en este arte nunca estuvo tan al alcance de la mano.',
     place: 'Calle 456',
-    monthlyPrice: 150,
+    weeklyPrice: 150,
     booking: {
       total: 10,
       remaining: 2
     },
-    timesOfActivity: [{
-      day: DaysOfWeek.Thursday,
-      startTime: '17:00',
-      endTime: '18:00'
-    }]
+    timesOfActivity: [
+      {
+        day: DaysOfWeek.Thursday,
+        startTime: createTime(17),
+        endTime: createTime(18)
+      },
+      {
+        day: DaysOfWeek.Saturday,
+        startTime: createTime(10),
+        endTime: createTime(11)
+      }
+    ]
   }
 ]
