da5d9652d0d1dd60a60882a25feb0dee45ef307a
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2022-11-18 22:20:55 -0300

    confiigured routes

--- hoteLink-backend/app.js
+++ hoteLink-backend/app.js
@@ -12,10 +12,44 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const init = require('./init');
+const activityRoutes = require('./routes/activityRoutes');
+const authRoutes = require('./routes/authRoutes');
+const customersRoutes = require('./routes/customersRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const hotelRoutes = require('./routes/activityRoutes');
+const imagesRoutes = require('./routes/imagesRoutes');
+const newsRoutes = require('./routes/newsRoutes');
+const restaurantRoutes = require('./routes/restaurantRoutes');
+const servicesRoutes = require('./routes/servicesRoutes');
 
 const app = express();
 
 app.use(init.setResponseAllowanceHeaders);
+
+const v1 = {
+  activityRoutes: 'activity',
+  authRoutes: 'auth',
+  customersRoutes: 'customers',
+  eventRoutes: 'event',
+  hotelRoutes: 'hotels',
+  imagesRoutes: 'images',
+  newsRoutes: 'news',
+  restaurantRoutes: 'restaurant',
+  servicesRoutes: 'services',
+}
+Object.entries(v1).forEach(([key, value]) => {
+  v1[key] = `/api/v1/${value}`;
+});
+
+app.use(v1.activityRoutes, activityRoutes);
+app.use(v1.authRoutes, authRoutes);
+app.use(v1.customersRoutes, customersRoutes);
+app.use(v1.eventRoutes, eventRoutes);
+app.use(v1.hotelRoutes, hotelRoutes);
+app.use(v1.imagesRoutes, imagesRoutes);
+app.use(v1.newsRoutes, newsRoutes);
+app.use(v1.restaurantRoutes, restaurantRoutes);
+app.use(v1.servicesRoutes, servicesRoutes);
 
 app.use(helmet());
 app.use(logger('combined'));
