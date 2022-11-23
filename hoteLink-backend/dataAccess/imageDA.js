a21fcb587b19a4c829694027caa1f9d6d388bb9d
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2022-11-23 15:03:14 -0300

    image backend

---
+++ hoteLink-backend/dataAccess/imageDA.js
@@ -0,0 +1,7 @@
+const Image = require('../entities/image')
+
+module.exports = {
+    getImage : async (imageId) => {
+        //retorna una instancia image, donde el path es el url. Undefined si no existe.
+    }
+}
